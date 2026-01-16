d4b3cddc6a6b31b2c8627ca098a467cd79df9c49
Nerwyn Singh <nerwyn1994@gmail.com>
2026-01-15 23:26:57 -0500

    add files

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # conky-cava-visualizer
-A conky visualizer made using Cava, Lua, and Cairo
+
+A visualizer made using Conky, Cava, Lua, and Cairo. No extra windows, just a conky visualizer on your desktop.
+
+## Setup
+
+1. Install [cava](https://github.com/karlstav/cava) using the instructions provided in its repository.
+2. Download this repository TODO add a release or something.
+3. Open a command line in the visualizer folder and run it using the command `conky -c ./visualizer.conf`.
+
+## Configuration
+
+Configuration for this visualizer lives in either the conky or cava config files depending on which settings it is relevant to.
+
+### visualizer.conf
+
+The conky configuration file.
+
+#### Height and Width
+
+Set `minimum_height` and `minimum_width` to your desired visualizer size. I found that I also needed to reduce width by 8px when making it full screen, otherwise conky would not load the configuration.
+
+#### Position
+
+Set `gap_x` and `gap_y` to reposition the visualizer. DO NOT use a non `_middle` alignment, doing so will break the visualizer lua script calculations.
+
+#### Monitor
+
+Set `xinerama_head` to the index of the monitor you want to display the visualizer on, starting with index 0.
+
+### Cava config
+
+The cava configuration file. Most options in this file are handled by cava itself. For those refer to the descriptions in file provided by the cava developers. Some additional options have been added or are used specifically by the conky visualizer lua script. You may also find the [cava example config file](https://github.com/karlstav/cava/blob/master/example_files/config) from its repository useful.
+
+#### Orientation
+
+[conky] orientation
+
+NOT the cava orientation value, which has been removed from this file and must be left at its default value. Can be `top`, `bottom`, `horizontal`, `left`, `right`, or `vertical`. Defaults to `bottom`.
+
+#### Color
+
+[color] color
+
+The visualizer bar color. Similar to the original cav foreground option. Must be a hex color.
+
+#### Opacity
+
+[color] opacity
+
+The opacity of the bars. Must be a float between 0 and 1.
---
+++ visualizer.lua
@@ -0,0 +1,159 @@
+-- Read config
+local inifile = require 'inifile'
+local config = inifile.parse('./config')
+
+local orientation = string.gsub(config['conky']['orientation'] or 'bottom', '%s+', '')
+local sideways = {'left', 'right', 'vertical'}
+local is_sideways = false
+for _, o in ipairs(sideways) do
+    if orientation == o then
+        is_sideways = true
+        break
+    end
+end
+
+local n_bars = tonumber(config['general']['bars'] or 512)
+local bar_spacing = tonumber(config['general']['bar_spacing'] or 1)
+local bar_max = tonumber(config['output']['ascii_max_range'] or 1000)
+
+local color = string.gsub(config['color']['color'] or '#FFF', '%s+', '')
+local opacity = tonumber(config['color']['opacity'] or 1)
+
+-- Cairo setup
+local cairo = require('cairo')
+local cairo_xlib = require('cairo_xlib')
+
+-- Hex color parsing https://gist.github.com/fernandohenriques/12661bf250c8c2d8047188222cab7e28
+function hex2rgb(hex)
+    local hex = hex:gsub("#", "")
+    if hex:len() == 3 then
+        return {(tonumber("0x" .. hex:sub(1, 1)) * 17) / 255, (tonumber("0x" .. hex:sub(2, 2)) * 17) / 255,
+                (tonumber("0x" .. hex:sub(3, 3)) * 17) / 255}
+    else
+        return {tonumber("0x" .. hex:sub(1, 2)) / 255, tonumber("0x" .. hex:sub(3, 4)) / 255,
+                tonumber("0x" .. hex:sub(5, 6)) / 255}
+    end
+end
+local rgb = hex2rgb(color)
+
+-- Cava pipe setup
+local pipe = io.popen('cava -p ./config', 'r')
+local last_line = ''
+function read_cava()
+    for line in pipe:lines() do
+        last_line = line
+        coroutine.yield()
+    end
+end
+local co = coroutine.create(read_cava)
+
+-- Visualizer mode setup
+local incrementors = {
+    horizontal = function(x, y, bar_width)
+        return x + bar_width + bar_spacing, y
+    end,
+
+    vertical = function(x, y, bar_width)
+        return x, y + bar_width + bar_spacing
+    end
+}
+local incrementor = incrementors[is_sideways and 'vertical' or 'horizontal']
+
+local bar_width_getters = {
+    horizontal = function()
+        return ((conky_window.width - bar_spacing) / n_bars) - bar_spacing
+    end,
+
+    vertical = function()
+        return ((conky_window.height - bar_spacing) / n_bars) - bar_spacing
+    end
+}
+local get_bar_width = bar_width_getters[is_sideways and 'vertical' or 'horizontal']
+
+local bar_height_getters = {
+    horizontal = function(i)
+        return math.floor((i / bar_max) * conky_window.height)
+    end,
+
+    vertical = function(i)
+        return math.floor((i / bar_max) * (conky_window.width))
+    end
+}
+local get_bar_height = bar_height_getters[is_sideways and 'vertical' or 'horizontal']
+
+function draw(cr, x0, y0, draw_bar)
+    local x = x0
+    local y = y0
+    local bar_width = get_bar_width()
+    for i in string.gmatch(last_line, "([^;]+)") do
+        i = tonumber(i or 0)
+        if i > 0 then
+            local bar_height = get_bar_height(i)
+            draw_bar(cr, x, y, bar_height, bar_width)
+        end
+        x, y = incrementor(x, y, bar_width)
+    end
+end
+
+local visualizers = {
+    top = function(cr)
+        draw(cr, bar_spacing, 0, function(cr, x, y, height, width)
+            cairo_rectangle(cr, x, y, width, height)
+        end)
+    end,
+
+    bottom = function(cr)
+        draw(cr, bar_spacing, conky_window.height, function(cr, x, y, height, width)
+            cairo_rectangle(cr, x, y - height, width, height)
+        end)
+    end,
+
+    horizontal = function(cr)
+        draw(cr, bar_spacing, math.floor(conky_window.height / 2), function(cr, x, y, height, width)
+            cairo_rectangle(cr, x, y - (height / 2), width, height)
+        end)
+    end,
+
+    left = function(cr)
+        draw(cr, 0, bar_spacing, function(cr, x, y, height, width)
+            cairo_rectangle(cr, x, y, height, width)
+        end)
+    end,
+
+    right = function(cr)
+        draw(cr, conky_window.width, bar_spacing, function(cr, x, y, height, width)
+            cairo_rectangle(cr, x - height, y, height, width)
+        end)
+    end,
+
+    vertical = function(cr)
+        draw(cr, math.floor(conky_window.width / 2), bar_spacing, function(cr, x, y, height, width)
+            cairo_rectangle(cr, x - (height / 2), y, height, width)
+        end)
+    end
+}
+local visualizer = visualizers[orientation] or draw_bottom
+
+-- Main method
+function conky_visualizer(n)
+    coroutine.resume(co)
+
+    if (conky_window.height or 0 > 0 and conky_window.width or 0 > 0) then
+        -- Cairo setup
+        local cs = cairo_xlib_surface_create(conky_window.display, conky_window.drawable, conky_window.visual,
+            conky_window.width, conky_window.height)
+        local cr = cairo_create(cs)
+        cairo_set_source_rgba(cr, rgb[1], rgb[2], rgb[3], opacity)
+
+        -- Draw using user selected visualizer
+        visualizer(cr)
+        cairo_fill(cr)
+
+        -- Cair teardown
+        cairo_destroy(cr)
+        cairo_surface_destroy(cs)
+        cr = nil
+    end
+
+    return ''
+end
